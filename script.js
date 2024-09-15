607561544672499817a1ca1f6c73b3730fa00bd5
bvnvn23 <oskarbanach25@gmail.com>
2024-09-15 17:23:02 +0200

    deleted unnecessary variable

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let computerBoard;
-
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.querySelector(".container");
     const startButton = document.querySelector(".play-button");
